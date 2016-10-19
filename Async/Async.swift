aa1cf6edf930f2dc3445bb62535a92bf3252779e
HJC <hjcapple@gmail.com>
2016-10-19 16:31:21 +0800

    使用 swiftformat 格式代码

--- Async/Async.swift
+++ Async/Async.swift
@@ -1,63 +1,56 @@
 /*
-The MIT License (MIT)
+ The MIT License (MIT)
 
-Copyright (c) 2016 HJC hjcapple@gmail.com
+ Copyright (c) 2016 HJC hjcapple@gmail.com
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 import Foundation
 
-public final class AsyncHolder<U>
-{
-    private var _leftBlock: (@escaping ((U)->Void)) -> Void
+public final class AsyncHolder<U> {
+    private var _leftBlock: (@escaping ((U) -> Void)) -> Void
     private var _isFinish = true
-    
-    fileprivate init(_ block: @escaping (@escaping ((U)->Void)) -> Void)
-    {
+
+    fileprivate init(_ block: @escaping (@escaping ((U) -> Void)) -> Void) {
         _leftBlock = block
     }
-    
-    deinit
-    {
-        if _isFinish
-        {
+
+    deinit {
+        if _isFinish {
             _leftBlock() { _ in
             }
         }
     }
-    
+
     @discardableResult
-    public func main<R>(_ rightBlock: @escaping (U)->R) -> AsyncHolder<R>
-    {
+    public func main<R>(_ rightBlock: @escaping (U) -> R) -> AsyncHolder<R> {
         return async(DispatchQueue.main, rightBlock)
     }
-    
+
     @discardableResult
-    public func background<R>(_ rightBlock: @escaping (U)->R) -> AsyncHolder<R>
-    {
+    public func background<R>(_ rightBlock: @escaping (U) -> R) -> AsyncHolder<R> {
         return async(DispatchQueue.global(), rightBlock)
     }
-    
+
     @discardableResult
-    public func async<R>(_ queue : DispatchQueue, _ rightBlock: @escaping (U)-> R) -> AsyncHolder<R>
-    {
+    public func async<R>(_ queue: DispatchQueue, _ rightBlock: @escaping (U) -> R) -> AsyncHolder<R> {
         _isFinish = false
         let leftBlock = _leftBlock
         return AsyncHolder<R> { complete in
@@ -69,16 +62,14 @@ public final class AsyncHolder<U>
             }
         }
     }
-    
+
     @discardableResult
-    public func main_delay<R>(_ time: Double, _ rightBlock: @escaping (U)->R) -> AsyncHolder<R>
-    {
+    public func main_delay<R>(_ time: Double, _ rightBlock: @escaping (U) -> R) -> AsyncHolder<R> {
         return delay(time, DispatchQueue.main, rightBlock)
     }
-    
+
     @discardableResult
-    public func delay<R>(_ time: Double, _ queue: DispatchQueue, _ rightBlock: @escaping (U)->R) -> AsyncHolder<R>
-    {
+    public func delay<R>(_ time: Double, _ queue: DispatchQueue, _ rightBlock: @escaping (U) -> R) -> AsyncHolder<R> {
         _isFinish = false
         let leftBlock = _leftBlock
         return AsyncHolder<R> { complete in
@@ -92,44 +83,37 @@ public final class AsyncHolder<U>
     }
 }
 
-public struct Async
-{
-    private static func empty() -> AsyncHolder<Void>
-    {
+public struct Async {
+
+    private static func empty() -> AsyncHolder<Void> {
         return AsyncHolder<Void> { complete in
             complete()
         }
     }
-    
+
     @discardableResult
-    public static func main<R>(_ rightBlock: @escaping (Void)->R) -> AsyncHolder<R>
-    {
+    public static func main<R>(_ rightBlock: @escaping (Void) -> R) -> AsyncHolder<R> {
         return empty().main(rightBlock)
     }
-    
+
     @discardableResult
-    public static func background<R>(_ rightBlock: @escaping (Void)->R) -> AsyncHolder<R>
-    {
+    public static func background<R>(_ rightBlock: @escaping (Void) -> R) -> AsyncHolder<R> {
         return empty().background(rightBlock)
     }
-    
+
     @discardableResult
-    public static func async<R>(_ queue : DispatchQueue, _ rightBlock: @escaping (Void)->R) -> AsyncHolder<R>
-    {
+    public static func async<R>(_ queue: DispatchQueue, _ rightBlock: @escaping (Void) -> R) -> AsyncHolder<R> {
         return empty().async(queue, rightBlock)
     }
-    
+
     @discardableResult
-    public static func main_delay<R>(_ time: Double, _ rightBlock: @escaping (Void)->R) -> AsyncHolder<R>
-    {
+    public static func main_delay<R>(_ time: Double, _ rightBlock: @escaping (Void) -> R) -> AsyncHolder<R> {
         return empty().main_delay(time, rightBlock)
     }
-    
+
     @discardableResult
-    public static func delay<R>(_ time: Double, _ queue: DispatchQueue, _ rightBlock: @escaping (Void)->R) -> AsyncHolder<R>
-    {
+    public static func delay<R>(_ time: Double, _ queue: DispatchQueue, _ rightBlock: @escaping (Void) -> R) -> AsyncHolder<R> {
         return empty().delay(time, queue, rightBlock)
     }
 }
-
 
